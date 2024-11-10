cd0b1096de8451091d58d3e1f566d16961a55469
NHViet03 <21522791@gm.uit.edu.vn>
2024-11-10 11:02:01 +0700

    Initialize Admin Source

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loading from "./components/Loading";
+import Alert from "./components/Alert";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import PageRender from "./customRouter/PageRender";
+
+import { useSelector, useDispatch } from "react-redux";
+import { refreshToken } from "./redux/actions/authAction";
+
+import moment from "moment";
+import "moment/locale/vi";
+moment.locale("vi");
 
 function App() {
+  const [showSideBar, setShowSideBar] = useState(true);
+  const loading = useSelector((state) => state.loading);
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    dispatch(refreshToken());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <div className="App">
+        {loading && <Loading />}
+        <Alert />
+
+        <SideBar showSideBar={showSideBar} />
+        <div
+          className="main_container"
+          style={{
+            marginLeft: showSideBar && auth.token ? "250px" : "0",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Header setShowSideBar={setShowSideBar} />
+          <div
+            style={{
+              flex: 1,
+            }}
+          >
+            <Routes>
+              <Route path="/" element={auth.token ? <Home /> : <Login />} />
+
+              <Route path="/:page" element={<PageRender />} />
+              <Route path="/:page/:id" element={<PageRender />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
