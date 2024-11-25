539469193cfae00a6e22b00179afaae4bd7fe53f
BichHaoHi <21522049@gm.uit.edu.vn>
2024-11-25 18:54:43 +0700

    Created actions and reducers in redux

---
+++ client/src/redux/reducers/index.js
@@ -0,0 +1,39 @@
+import { combineReducers } from "redux";
+import auth from "./authReducer";
+import postDetail from "./postDetailReducer";
+import sharePost from "./sharePostReducer";
+import theme from "./themeReducer";
+import developer from "./developerReducer";
+import homePosts from "./postReducer";
+import notify from "./notifyReducer";
+import addPostModal from "./addPostModalReducer";
+import alert from "./alertReducer";
+import suggest from "./suggestReducer";
+import explore from "./exploreReducer";
+import profile from "./profileReducer";
+import socket from "./socketReducer";
+import modal from "./modalReducer";
+import message from "./messageReducer";
+import online from "./onlineReducer";
+import call from "./callReducer";
+import peer from "./peerReducer";
+export default combineReducers({
+  auth,
+  alert,
+  postDetail,
+  sharePost,
+  theme,
+  developer,
+  homePosts,
+  notify,
+  addPostModal,
+  suggest,
+  explore,
+  profile,
+  socket,
+  modal,
+  message,
+  online,
+  call,
+  peer
+});
